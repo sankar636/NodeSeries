8d79f8689dbc189281ab7efdfa4c1d127d576032
sankarshan <ssankarshan636@gmail.com>
2025-05-18 01:09:40 +0530

    Implement HTTP server with logging functionality

--- index.js
+++ index.js
@@ -34,13 +34,71 @@ console.log('Jai Sri Ram')
 
 // (7)
 // Anonimous function
-const Module = require('./Module.js')
-console.log(Module);
+// const Module = require('./Module.js')
+// console.log(Module);
 // this prints { add: [Function (anonymous)], sub: [Function (anonymous)] } 
 
 // Note --> Also Node.js has their own build in packages that we can import as per required. Example --> 'fs', 'http', 'crypto' etc
 
-const fs = require("fs")
+// const fs = require("fs")
 
 
+
+// -------------------------------------------------------------------------------------------
+const http = require('http')
+
+//(1) -> a
+/* const myServer = http.createServer((req, res) => {
+   console.log(req.headers); // req is a object which store information about where the data comes form 
+    res.end("Server Running")
+ }) 
+*/
+
+
+//(1) -> b
+/*
+const myServer = http.createServer((req, res) => {
+    if (Object.keys(req.headers).length === 0) {
+        console.log("No headers received.");
+        } else {
+            console.log(req.headers);
+    }
+    res.end("Server Running");
+    })
+*/   
+
+// (2)
+/*
+const fs = require('fs')
+const myServer = http.createServer((req, res) => {
+   const log = `${Date.now()}: new request received\n`
+   fs.appendFile('log text',log, (err, data) => {
+       res.end("Server Running")
+   })
+})
+*/
+
+// It will append a file name log.text and inside it he store Date.now: new request received. Any time the server refresh it store the data
+
+// (4)
+const fs = require('fs')
+const myServer = http.createServer((req, res) => {
+   const log = `${Date.now()}:${req.url} new request received\n`
+   fs.appendFile('log text',log, (err, data) => {
+    switch (req.url) {
+        case '/':
+            res.end("End Pare")            
+            break;
+        case '/about':
+            res.end("I am Sankarshan Sahu")            
+            break;    
+        default:
+            res.end("404 Not Found")            
+            break;
+    }
+   })
+})
+myServer.listen(8000, () => console.log("Server Started!"))
+
+// here we add the path 
 
